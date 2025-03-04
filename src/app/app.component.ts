6022f3b8ab4568c3c2dc790f70cf56dbeb245ab4
KyleFlynn1 <158482875+KyleFlynn1@users.noreply.github.com>
2025-03-04 15:47:58 +0000

    Week 7
    Working work from Week 7 First Part

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'Movie Finder';
+  movieData: IOMDBResponse | undefined;
+  errorMessage:any;
+
+  constructor(private _omdbService:OmdbApiService){}
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData
+        console.log("Director Name : " + this.movieData.Director);
+      }
+    )
+
+    return false;
+  }
+  
+}
